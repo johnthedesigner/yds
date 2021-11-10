cd91f8d7b4242438e541a0d35543e9003f87267d
johnthedesigner <john@johnthedesigner.com>
2021-11-09 22:32:52 -0500

    Change config to use either import.meta.env or process.env

--- shopify.config.js
+++ shopify.config.js
@@ -1,6 +1,18 @@
+var storeDomain = '';
+var storefrontToken = '';
+if (import.meta && import.meta.env) {
+  // Found import.meta
+  storeDomain = import.meta.env.VITE_SHOPIFY_DOMAIN;
+  storefrontToken = import.meta.env.VITE_SHOPIFY_TOKEN;
+} else if (process.env) {
+  // Found process.env
+  storeDomain = process.env.VITE_SHOPIFY_DOMAIN;
+  storefrontToken = process.env.VITE_SHOPIFY_TOKEN;
+}
+
 export default {
   locale: 'en-us',
-  storeDomain: import.meta.env ? import.meta.env.VITE_SHOPIFY_DOMAIN : '',
-  storefrontToken: import.meta.env ? import.meta.env.VITE_SHOPIFY_TOKEN : '',
+  storeDomain,
+  storefrontToken,
   graphqlApiVersion: 'unstable',
 };
