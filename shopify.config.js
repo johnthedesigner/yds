c1f4bf9d40103a389e4903cbdcc5a2b4f09ac6f0
johnthedesigner <john@johnthedesigner.com>
2021-11-13 15:31:50 -0500

    favor using process.env for shopify vars

--- shopify.config.js
+++ shopify.config.js
@@ -4,7 +4,7 @@ if (import.meta && import.meta.env) {
   // Found import.meta
   storeDomain = import.meta.env.VITE_SHOPIFY_DOMAIN;
   storefrontToken = import.meta.env.VITE_SHOPIFY_TOKEN;
-} else if (process.env) {
+} else {
   // Found process.env
   storeDomain = process.env.VITE_SHOPIFY_DOMAIN;
   storefrontToken = process.env.VITE_SHOPIFY_TOKEN;
