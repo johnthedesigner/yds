1f3d62213877eae110a88bcd1b1767d6ad9118e2
johnthedesigner <john@johnthedesigner.com>
2021-11-21 12:43:20 -0500

    Removing some elements and adding YDS colors

--- src/pages/Index.server.jsx
+++ src/pages/Index.server.jsx
@@ -10,62 +10,6 @@ import {Link} from '../components/Link.client';
 import Layout from '../components/Layout.server';
 import FeaturedCollection from '../components/FeaturedCollection.server';
 import ProductCard from '../components/ProductCard.server';
-import Welcome from '../components/Welcome.server';
-
-function GradientBackground() {
-  return (
-    <div className="fixed top-0 w-full h-3/5 overflow-hidden">
-      <div className="absolute w-full h-full bg-gradient-to-t from-gray-50 z-10" />
-
-      <svg
-        viewBox="0 0 960 743"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        className="filter blur-[30px]"
-        aria-hidden="true"
-      >
-        <defs>
-          <path fill="#fff" d="M0 0h960v540H0z" id="reuse-0" />
-        </defs>
-        <g clipPath="url(#a)">
-          <use xlinkHref="#reuse-0" />
-          <path d="M960 0H0v743h960V0Z" fill="#7CFBEE" />
-          <path
-            d="M831 380c200.48 0 363-162.521 363-363s-162.52-363-363-363c-200.479 0-363 162.521-363 363s162.521 363 363 363Z"
-            fill="#4F98D0"
-          />
-          <path
-            d="M579 759c200.479 0 363-162.521 363-363S779.479 33 579 33 216 195.521 216 396s162.521 363 363 363Z"
-            fill="#7CFBEE"
-          />
-          <path
-            d="M178 691c200.479 0 363-162.521 363-363S378.479-35 178-35c-200.4794 0-363 162.521-363 363s162.5206 363 363 363Z"
-            fill="#4F98D0"
-          />
-          <path
-            d="M490 414c200.479 0 363-162.521 363-363S690.479-312 490-312 127-149.479 127 51s162.521 363 363 363Z"
-            fill="#4F98D0"
-          />
-          <path
-            d="M354 569c200.479 0 363-162.521 363-363 0-200.47937-162.521-363-363-363S-9 5.52063-9 206c0 200.479 162.521 363 363 363Z"
-            fill="#7CFBEE"
-          />
-          <path
-            d="M630 532c200.479 0 363-162.521 363-363 0-200.4794-162.521-363-363-363S267-31.4794 267 169c0 200.479 162.521 363 363 363Z"
-            fill="#4F98D0"
-          />
-        </g>
-        <path fill="#fff" d="M0 540h960v203H0z" />
-        <defs>
-          <clipPath id="a">
-            <use xlinkHref="#reuse-0" />
-          </clipPath>
-        </defs>
-      </svg>
-    </div>
-  );
-}
 
 export default function Index({country = {isoCode: 'US'}}) {
   const {data} = useShopQuery({
@@ -84,13 +28,14 @@ export default function Index({country = {isoCode: 'US'}}) {
     collections && collections.length > 1 ? collections[1] : collections[0];
 
   return (
-    <Layout hero={<GradientBackground />}>
+    // Temporarily remove the homepage hero
+    // <Layout hero={<GradientBackground />}>
+    <Layout>
       <div className="relative mb-12">
-        <Welcome />
         <div className="bg-white p-12 shadow-xl rounded-xl mb-10">
           {featuredProductsCollection ? (
             <>
-              <div className="flex justify-between items-center mb-8 text-md font-medium">
+              <div className="flex justify-between items-center mb-8 text-md font-serif">
                 <span className="text-black uppercase">
                   {featuredProductsCollection.title}
                 </span>
