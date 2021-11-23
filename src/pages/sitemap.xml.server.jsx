afc881cbea5fa4add4cdf4c1bbec78aa07edba05
johnthedesigner <john@johnthedesigner.com>
2021-11-23 16:49:32 -0500

    Test sitemap update

--- src/pages/sitemap.xml.server.jsx
+++ src/pages/sitemap.xml.server.jsx
@@ -16,6 +16,7 @@ function shopSitemap(data) {
     <urlset
       xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
       xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+      apple="orange"
     >
       ${flattenConnection(data.products)
         .map((product) => {
