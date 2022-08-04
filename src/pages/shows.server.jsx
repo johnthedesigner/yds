3dfda7f71f26d2caea785c7839f83b22ce648eea
johnthedesigner <john@johnthedesigner.com>
2022-08-04 13:42:04 -0400

    starting to fetch from contentful

--- src/pages/shows.server.jsx
+++ src/pages/shows.server.jsx
@@ -1,3 +1,10 @@
+import {fetchSync, CacheLong, useQuery} from '@shopify/hydrogen';
+import {Suspense} from 'react';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+import _ from 'lodash';
+import contentful from 'contentful';
+
 import Layout from '../components/Layout.server';
 import Hero from '../components/Hero.server';
 import ShowList from '../components/ShowList.client';
@@ -17,14 +24,56 @@ const Shows = ({response}) => {
     noStore: true,
   });
 
+  const [events, setEvents] = useState(null);
+
+  const {VITE_CONTENT_SPACE_ID, VITE_CONTENT_TOKEN, VITE_CONTENT_ENTRY_ID} =
+    process.env;
+
+  const client = contentful.createClient({
+    space: VITE_CONTENT_SPACE_ID,
+    accessToken: VITE_CONTENT_TOKEN,
+  });
+
+  // useEffect(() => {
+  console.log('prepare to fetch events');
+  const {data} = useQuery('getEvents', async () => {
+    // const response = await fetch(
+    //   `https://cdn.contentful.com/spaces/${VITE_CONTENT_SPACE_ID}/environments/master/entries/${VITE_CONTENT_ENTRY_ID}?access_token=${VITE_CONTENT_TOKEN}`,
+    //   {
+    //     headers: {
+    //       accept: 'application/json',
+    //     },
+    //   },
+    // );
+    // return await response.json();
+    client.getEntries().then(function (entries) {
+      // log the title for all the entries that have it
+      entries.items.forEach(function (entry) {
+        if (entry.fields.name) {
+          console.log(entry.fields.name);
+        }
+      });
+    });
+  });
+  console.log(data);
+  // setEvents(data);
+  // }, []);
+
+  // console.log(events);
+
   return (
     <Layout
       hero={<Hero title="Dahlia Shows" image="/purple-flowers.jpg" />}
       isCommercePage={false}
     >
+      {_.map(events, (event) => {
+        return <p>{event.name}</p>;
+      })}
       <NewSeo page={pages.shows} />
       <Bumper text="Show dates, locations and info will be updated as they are announced by their respective clubs." />
-      <ShowList />
+      <Suspense>
+        <ShowList />
+      </Suspense>
     </Layout>
   );
 };
