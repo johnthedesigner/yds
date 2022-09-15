44640d37ad3a663fece98200e2c1ec319a3c8591
johnthedesigner <john@johnthedesigner.com>
2022-09-15 13:09:52 -0400

    increase request limit

--- utils/strapi.js
+++ utils/strapi.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 const { STRAPI_TOKEN, STRAPI_API } = process.env;
 
+const requestLimit = 100;
+
 export const getCollection = async (contentType) => {
-  let requestUrl = `${STRAPI_API}${contentType}/?populate=%2A`;
+  let requestUrl = `${STRAPI_API}${contentType}/?populate=%2A&pagination[page]=1&pagination[pageSize]=${requestLimit}`;
 
   let { data } = await axios.get(requestUrl, {
     headers: {
